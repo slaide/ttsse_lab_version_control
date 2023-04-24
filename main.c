a7baab9f21d397e6f96facf8804f248ed36d94e5
slaide <patrick_hennig@fastmail.com>
2023-04-24 13:51:25 +0200

    add helper function

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(int argc, char**argv){
+
+void printhelper(){
     printf("hello from the ttsse lab on version control\n");
+}
+
+int main(int argc, char **argv)
+{
+    printhelper();
+
     return 0;
 }
