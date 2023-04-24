cb797c1ce0345c06895d70e5c1a68aec901ee907
slaide <patrick_hennig@fastmail.com>
2023-04-24 14:07:03 +0200

    add opinions on the lab

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 
 void printhelper()
 {
-    printf("hello from the ttsse lab on version control\n");
+    printf(
+        "hello from the ttsse lab on version control\n"
+        "this i a grewat lab\n"
+        "ive learned aboout useful git fettures\n"
+        "like stash\n"
+        "also, the anshtrom campus loosk nice. love to hugr windowse.\n"
+    );
 }
 
 int main(int argc, char **argv)
